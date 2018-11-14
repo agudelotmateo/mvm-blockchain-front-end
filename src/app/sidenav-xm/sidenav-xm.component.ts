56fca89676dee4c62c3f51011bb07d17b60e206a
Juanesfon5 <juanesfon55@hotmail.com>
2018-11-14 02:14:29 -0500

    Menu Fixes

--- src/app/sidenav-xm/sidenav-xm.component.ts
+++ src/app/sidenav-xm/sidenav-xm.component.ts
@@ -7,15 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavXmComponent implements OnInit {
   agent = false;
-
+  regulator = true;
   constructor() { }
 
   ngOnInit() {
     let ss = JSON.parse(localStorage.getItem('user'));
     if(ss.type == "agent"){
       this.agent = true;
+      this.regulator = false;
     } else {
       this.agent = false;
+      this.regulator = true;
     }
   }
 
